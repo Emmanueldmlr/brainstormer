8653348fc702dba9e7d44d477b7e9b8db3521c3a
OleanjiKingCode <holamilean56@gmail.com>
2023-03-17 17:27:36 +0100

    unwanted imports

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,31 +1,13 @@
-import { StepsStyleConfig } from 'chakra-ui-steps'
-import { ChakraTheme } from '@chakra-ui/react'
-import { StyleFunctionProps } from '@chakra-ui/theme-tools'
-import { Avatar } from './Avatar'
-import { Button } from './Button'
-import { Input } from './Input'
-import { Link } from './Link'
+import { ChakraTheme } from "@chakra-ui/react";
+import { StyleFunctionProps } from "@chakra-ui/theme-tools";
+import { Avatar } from "./Avatar";
+import { Button } from "./Button";
+import { Input } from "./Input";
+import { Link } from "./Link";
 
-const CustomSteps = {
-  ...StepsStyleConfig,
-  baseStyle: (props: StyleFunctionProps) => {
-    return {
-      ...StepsStyleConfig.baseStyle(props),
-      connector: {
-        ...StepsStyleConfig.baseStyle(props).connector,
-        // your custom styles here
-        left: '50%',
-        width: '100px',
-        right: '50%',
-      },
-    }
-  },
-}
-
-export const components: ChakraTheme['components'] = {
+export const components: ChakraTheme["components"] = {
   Avatar,
   Button,
   Input,
   Link,
-  Steps: CustomSteps,
-}
+};
